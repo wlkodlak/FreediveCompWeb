e0afab51a335d15c5cff5c74152be6bf6f6cb545
Milan Wilczak <milan.wilczak@gmail.com>
2018-10-12 10:30:45 +0200

    Autodetect remote API

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,6 +2,16 @@ import MockedApi from './MockedApi';
 import RemoteApi from './RemoteApi';
 import TokenStorage from './TokenStorage';
 
-const apiBaseUrl = process.env.REACT_APP_API;
-const Api = apiBaseUrl ? new RemoteApi(apiBaseUrl, new TokenStorage()) : new MockedApi();
+function createApi() {
+  let apiBaseUrl = process.env.REACT_APP_API;
+  if (apiBaseUrl === "mocked") return new MockedApi();
+  if (apiBaseUrl === "auto" || !apiBaseUrl) {
+    const regex = /^(https?:\/\/[^\/]*)/;
+    const match = regex.exec(window.location.href);
+    apiBaseUrl = match[1];
+  }
+  return new RemoteApi(apiBaseUrl, new TokenStorage());
+}
+
+const Api = createApi();
 export default Api;
