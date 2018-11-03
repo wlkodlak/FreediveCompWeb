ab66710d3bbcab3816036067720806c8810b2142
Milan Wilczak <milan.wilczak@gmail.com>
2018-11-03 14:28:14 +0100

    Basic anonymous mode

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { H1, H2, UL, Toaster, Toast, Intent } from '@blueprintjs/core';
 import Api from '../../api/Api';
 
@@ -58,41 +58,85 @@ class HomePage extends React.Component {
   }
 
   render() {
-    const raceId = this.props.raceId;
-    const title = this.state.title;
     const userType = this.state.userType;
     if (userType === "Anonymous") {
-      return (<Redirect to={`/${raceId}/authenticate`} />);
+      return this.renderAnonymous();
     } else if (userType === "Judge") {
-      return (<Redirect to={`/${raceId}/startinglists`} />);
+      return this.renderJudge();
     } else if (userType === "Admin") {
-      return (
-        <div className="homepage">
-          <Toaster>{ this.state.errors.map((error, index) => <Toast intent={Intent.DANGER} message={error} onDismiss={() => this.onErrorDismissed(index)} />) }</Toaster>
-          <H1>{title}</H1>
-          <H2>Competition progress</H2>
-          <UL>
-            <li><Link to={`/${raceId}/startinglists`}>Start lists</Link></li>
-            <li><Link to={`/${raceId}/disciplines`}>Disciplines</Link></li>
-            <li><Link to={`/${raceId}/resultlists`}>Results lists</Link></li>
-            <li><Link to={`/${raceId}/startinglists/generator`}>Generate start list</Link></li>
-          </UL>
-          <H2>Competition setup</H2>
-          <UL>
-            <li><Link to={`/${raceId}/setup`}>Settings</Link></li>
-            <li><Link to={`/${raceId}/judges`}>Judges</Link></li>
-            <li><Link to={`/${raceId}/athletes`}>Athletes</Link></li>
-          </UL>
-        </div>
-      );
+      return this.renderAdmin();
     } else {
-      return (
-        <div className="homepage">
-          <Toaster>{ this.state.errors.map((error, index) => <Toast intent={Intent.DANGER} message={error} onDismiss={() => this.onErrorDismissed(index)} />) }</Toaster>
-          <H1>{title}</H1>
-        </div>
-      );
+      return this.renderUnknown();
     }
+  }
+
+  renderAnonymous() {
+    const raceId = this.props.raceId;
+    const title = this.state.title;
+    return (
+      <div className="homepage">
+        <Toaster>{ this.state.errors.map((error, index) => <Toast intent={Intent.DANGER} message={error} onDismiss={() => this.onErrorDismissed(index)} />) }</Toaster>
+        <H1>{title}</H1>
+        <UL>
+          <li><Link to={`/${raceId}/startinglists`}>Start lists</Link></li>
+          <li><Link to={`/${raceId}/disciplines`}>Disciplines</Link></li>
+          <li><Link to={`/${raceId}/resultlists`}>Results lists</Link></li>
+          <li><Link to={`/${raceId}/athletes`}>Athletes</Link></li>
+          <li><Link to={`/${raceId}/authenticate`}>Authenticate as judge</Link></li>
+        </UL>
+      </div>
+    );
+  }
+
+  renderJudge() {
+    const raceId = this.props.raceId;
+    const title = this.state.title;
+    return (
+      <div className="homepage">
+        <Toaster>{ this.state.errors.map((error, index) => <Toast intent={Intent.DANGER} message={error} onDismiss={() => this.onErrorDismissed(index)} />) }</Toaster>
+        <H1>{title}</H1>
+        <UL>
+          <li><Link to={`/${raceId}/startinglists`}>Start lists</Link></li>
+          <li><Link to={`/${raceId}/disciplines`}>Disciplines</Link></li>
+          <li><Link to={`/${raceId}/resultlists`}>Results lists</Link></li>
+          <li><Link to={`/${raceId}/athletes`}>Athletes</Link></li>
+        </UL>
+      </div>
+    );
+  }
+
+  renderAdmin() {
+    const raceId = this.props.raceId;
+    const title = this.state.title;
+    return (
+      <div className="homepage">
+        <Toaster>{ this.state.errors.map((error, index) => <Toast intent={Intent.DANGER} message={error} onDismiss={() => this.onErrorDismissed(index)} />) }</Toaster>
+        <H1>{title}</H1>
+        <H2>Progress</H2>
+        <UL>
+          <li><Link to={`/${raceId}/startinglists`}>Start lists</Link></li>
+          <li><Link to={`/${raceId}/disciplines`}>Disciplines</Link></li>
+          <li><Link to={`/${raceId}/resultlists`}>Results lists</Link></li>
+        </UL>
+        <H2>Setup</H2>
+        <UL>
+          <li><Link to={`/${raceId}/setup`}>Settings</Link></li>
+          <li><Link to={`/${raceId}/judges`}>Judges</Link></li>
+          <li><Link to={`/${raceId}/athletes`}>Athletes</Link></li>
+          <li><Link to={`/${raceId}/startinglists/generator`}>Generate start list</Link></li>
+        </UL>
+      </div>
+    );
+  }
+
+  renderUnknown() {
+    const title = this.state.title;
+    return (
+      <div className="homepage">
+        <Toaster>{ this.state.errors.map((error, index) => <Toast intent={Intent.DANGER} message={error} onDismiss={() => this.onErrorDismissed(index)} />) }</Toaster>
+        <H1>{title}</H1>
+      </div>
+    );
   }
 }
 
