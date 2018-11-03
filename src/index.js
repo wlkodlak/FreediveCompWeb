583fc510e7c1f6606d9653f3aa18514c4a643fe9
Milan Wilczak <milan.wilczak@gmail.com>
2018-11-03 11:01:37 +0100

    Get rid of service-worker (it causes only trouble)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'normalize.css/normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
@@ -11,4 +10,3 @@ import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 ReactDOM.render(
   <App />,
   document.getElementById('root'));
-registerServiceWorker();
