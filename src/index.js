71d6dd9b9121f8fbb12e4622cf78f4b5b3bd5340
Milan Wilczak <milan.wilczak@gmail.com>
2018-11-05 12:53:58 +0100

    Use better way of disabling service worker (ensures that is it not used even if it was before).

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as registerServiceWorker from './registerServiceWorker';
 import 'normalize.css/normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
@@ -11,4 +11,4 @@ import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 ReactDOM.render(
   <App />,
   document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker.unregister();
