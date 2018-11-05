b8baabaf8e08f7e45d932ac025a8a1cc476c5a29
Milan Wilczak <milan.wilczak@gmail.com>
2018-11-05 12:49:46 +0100

    Revert "Get rid of service-worker (it causes only trouble)"
    This reverts commit 583fc510e7c1f6606d9653f3aa18514c4a643fe9.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 import 'normalize.css/normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
@@ -10,3 +11,4 @@ import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 ReactDOM.render(
   <App />,
   document.getElementById('root'));
+registerServiceWorker();
